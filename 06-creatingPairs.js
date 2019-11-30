4e6184a8e1d907fffc976536ff5c31f9d4cbd7c0
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-11-30 15:42:40 -0800

    completed Creating Pairs

---
+++ 06-creatingPairs.js
@@ -0,0 +1,13 @@
+// What is the Big O of the following algorithm? Explain your answer
+
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+
+// O(n^k) Polynomial time
+// This algorithm requires 2 levels of looping over the input array
+// The runtime is O(n^2).
