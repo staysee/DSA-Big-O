163b677dd285314039787f982aac3adb83dc042f
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-11-30 19:21:15 -0800

    complete Compute the Sequence

---
+++ 07-ComputeTheSequence.js
@@ -0,0 +1,24 @@
+//What does the following algorithm do? What is its runtime complexity? Explain your answer
+
+function compute(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+
+        if (i === 1) {
+            result.push(0);
+        }
+        else if (i == 2) {
+            result.push(1);
+        }
+        else {
+            result.push(result[i - 2] + result[i - 3]);
+        }
+    }
+    return result;
+}
+
+// O(n) Linear Time
+// This is computing the Fibonacci Sequence!
+// This is a linear way to solve the fibonacci sequence.
+// The algorithm computes the sum of 2 numbers and pushes it into the array
+
